6325e393b9cc2e31a99470b20973aba62b0f2736
pham.tuan.anhc <tuananhchums23@gmail.com>
2024-07-25 14:58:26 +0700

    init db

---
+++ db/migrate/20240725033512_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :content
+      t.boolean :status
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+
+    add_index :posts, %i[user_id created_at]
+  end
+end
